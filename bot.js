f97863483166567c3707050bf5f893b8e8e5bb03
Saitama <badaoui.iliess@gmail.com>
2024-09-30 16:21:43 +0200

    maj colissimo

--- bot.js
+++ bot.js
@@ -8,22 +8,22 @@ async function startAutomation() {
   logger.info('Démarrage de toutes les opérations des bots...');
 
   // Lancer Chronopost Export indépendamment
-  // try {
-  //   logger.info('Lancement de l\'export de Chronopost...');
-  //   await loginChronopostExport();
-  //   logger.info('Export de Chronopost terminé avec succès.');
-  // } catch (error) {
-  //   logger.warn('Erreur ou absence de nouvelles réclamations lors de l\'export de Chronopost :', error.message);
-  // }
+  try {
+    logger.info('Lancement de l\'export de Chronopost...');
+    await loginChronopostExport();
+    logger.info('Export de Chronopost terminé avec succès.');
+  } catch (error) {
+    logger.warn('Erreur ou absence de nouvelles réclamations lors de l\'export de Chronopost :', error.message);
+  }
 
   // Lancer Chronopost Import indépendamment
-  // try {
-  //   logger.info('Lancement de l\'import de Chronopost...');
-  //   await loginChronopostImport();
-  //   logger.info('Import de Chronopost terminé avec succès.');
-  // } catch (error) {
-  //   logger.warn('Erreur lors de l\'import de Chronopost :', error.message);
-  // } 
+  try {
+    logger.info('Lancement de l\'import de Chronopost...');
+    await loginChronopostImport();
+    logger.info('Import de Chronopost terminé avec succès.');
+  } catch (error) {
+    logger.warn('Erreur lors de l\'import de Chronopost :', error.message);
+  } 
 
   // Lancer Colissimo Export indépendamment
   try {
@@ -35,13 +35,13 @@ async function startAutomation() {
   }
 
   // Lancer Colissimo Import indépendamment
-  // try {
-  //   logger.info('Lancement de l\'import de Colissimo...');
-  //   await loginColissimoImport();
-  //   logger.info('Import de Colissimo terminé avec succès.');
-  // } catch (error) {
-  //   logger.warn('Erreur lors de l\'import de Colissimo :', error.message);
-  // }
+  try {
+    logger.info('Lancement de l\'import de Colissimo...');
+    await loginColissimoImport();
+    logger.info('Import de Colissimo terminé avec succès.');
+  } catch (error) {
+    logger.warn('Erreur lors de l\'import de Colissimo :', error.message);
+  }
   
 
   
